4ee6c46ecbeda0fa265817ec65943b3efd7a3249
Tim La <tlamarketing111@gmail.com>
2023-01-06 23:51:05 -0500

    imported imports for app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { Typography } from '@material-ui/core'
+import { Typography, AppBar, Card, CardActions, CardContent, CardMedia, CssBaseline, Grid, Toolbar, Container } from '@material-ui/core'
+import PhotoCamera from '@material-ui/icons'
 
 const App = () => {
     return (
