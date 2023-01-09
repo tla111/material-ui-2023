45a732cd412bd3150ee25eee2d0033767927f3b8
Tim La <tlamarketing111@gmail.com>
2023-01-09 00:31:04 -0500

    styled container

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,7 +4,10 @@ import { PhotoCamera } from '@material-ui/icons'
 import { makeStyles } from '@material-ui/core/styles'
 
 const useStyles = makeStyles((theme) => ({
-
+    container: {
+        backgroundColor: theme.palette.background.paper,
+        padding: theme.spacing(8, 0, 6)
+    }
 }))
 
 
