27ad95ed33d67d7c91a14af1845574b81d2745af
Tim La <tlamarketing111@gmail.com>
2023-01-11 07:32:44 -0500

    styled card

--- frontend/src/styles.js
+++ frontend/src/styles.js
@@ -12,16 +12,18 @@ const useStyles = makeStyles((theme) => ({
         marginTop: '40px',
     },
     cardGrid: {
-
+        padding: '20px 0'
     },
     card: {
-
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
     },
     cardMedia: {
-
+        paddingTop: '56.25%'
     },
     cardContent: {
-
+        flexGrow: 1,
     },
 }))
 
