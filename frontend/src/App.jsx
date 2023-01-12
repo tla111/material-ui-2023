95c8fade8dd3152259ff94a4d080248622f87500
Tim La <tlamarketing111@gmail.com>
2023-01-12 08:32:57 -0500

    page is now responsive

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -47,7 +47,7 @@ const App = () => {
                 <Container className={classes.cardGrid} maxWidth='md'>
                     <Grid container spacing={4}>
                         {cards.map((card) => (
-                            <Grid item key={card}>
+                            <Grid item key={card} xs={12} sm={6} md={4}>
                                 <Card className={classes.card}>
                                     <CardMedia className={classes.cardMedia} image='https://source.unsplash.com/random' />
                                     <CardContent className={classes.CardContent}>
